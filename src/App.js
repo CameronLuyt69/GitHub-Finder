527ad43fedd3d1ce3ac26a2dfd861ad446d637bc
CAMERON LUYT <cameronluyt30@gmail.com>
2020-06-13 15:35:01 +0200

    GitHub Finder

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import NavBar from './Components/Layout/NavBar';
+import User from './Components/Users/User';
+import Alert from './Components/Layout/Alert';
+import About from './Components/Pages/About';
+import NotFound from './Components/Pages/NotFound';
+import Home from './Components/Pages/Home';
+import GithubState from './Context/github/GithubState';
+import AlertState from './Context/alert/AlertState';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GithubState>
+      <AlertState>
+        <Router>
+          <div className="App">
+            <NavBar />
+            <div className='container'>
+              <Alert />
+              <Switch>
+                <Route exact path='/' component={Home} />
+                  <Route exact path='/About' component={About} />
+                  <Route exact path='/user/:login' component={User} />
+                  <Route component={NotFound} />
+              </Switch>
+            </div>
+          </div>
+        </Router>
+      </AlertState>
+    </GithubState>
   );
 }
 
---
+++ src/Components/Users/Search.js
@@ -0,0 +1,41 @@
+import React, { useState, useContext } from 'react';
+import githubContext from '../../Context/github/githubContext';
+import AlertContext from '../../Context/alert/alertContext';
+
+const Search = () => {
+    const GithubContext = useContext(githubContext);
+    const alertContext = useContext(AlertContext);
+
+    const [text, setText] = useState('');
+
+    const onSubmit = e => {
+        e.preventDefault();
+        if(text === '') {
+            alertContext.setAlert('Please enter something', 'light');
+        } else {
+        GithubContext.searchUsers(text);
+        setText('');
+        }
+    }
+
+    const onChange = e => setText(e.target.value);
+
+    return (
+        <div>
+            <form className='form' onSubmit={onSubmit}>
+                <input type='text' name='text' placeholder='Search Users...' value={text} onChange={onChange} />
+                <input type='submit' value='search' className='btn btn-dark btn-block'  />
+            </form>
+            {GithubContext.users.length > 0  && (
+            <button 
+            className="btn btn-light btn-block"
+            onClick={GithubContext.clearUsers}>
+                Clear
+            </button>
+            )}
+        </div>
+    )
+    
+}
+
+export default Search
